8eb531952c9813250c61d83c2b96e3bd305f662b
yugmerabtene <yug.merabtene@gmail.com>
2023-11-12 22:07:43 +0100

    Library
    Library

--- 1-GRASP/src/grasp/Book.java
+++ 1-GRASP/src/grasp/Book.java
@@ -1,28 +1,28 @@
+// Book.java
 package grasp;
 
-//Book.java
 public class Book {
- private String title;
- private boolean available;
+    private String title;
+    private boolean available;
 
- // Constructeur pour initialiser le livre avec un titre et le marquer comme disponible
- public Book(String title) {
-     this.title = title;
-     this.available = true;
- }
+    // Constructeur pour initialiser le livre avec un titre et le marquer comme disponible
+    public Book(String title) {
+        this.title = title;
+        this.available = true;
+    }
 
- // Méthode pour obtenir le titre du livre
- public String getTitle() {
-     return title;
- }
+    // Getter pour obtenir le titre du livre
+    public String getTitle() {
+        return title;
+    }
 
- // Méthode pour vérifier si le livre est disponible
- public boolean isAvailable() {
-     return available;
- }
+    // Getter pour vérifier si le livre est disponible
+    public boolean isAvailable() {
+        return available;
+    }
 
- // Méthode pour définir l'état de disponibilité du livre
- public void setAvailable(boolean available) {
-     this.available = available;
- }
+    // Setter pour définir l'état de disponibilité du livre
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
